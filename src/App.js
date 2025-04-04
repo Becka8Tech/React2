fad595e42a35350d30edfbea0767519cc9bcfdc3
Becka8Tech <rebekahmiller18@gmail.com>
2025-04-04 15:16:02 -0600

    initialize

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import Footer from './components/Footer.jsx';
+import Nav from './components/Nav.jsx';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Books from './pages/Books.jsx';
+import { books } from "./data";
+import BookInfo from './pages/BookInfo.jsx';
+
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <Nav />
+      <Route path="/" exact component={Home} />
+      <Route path="/books" render={() => <Books books={books} />} />
+      <Route path="/books/1" render={() => <BookInfo books={books} />} />
+      <Footer />
+    </div>
+    </Router>
+  );
+}
+
+export default App;
